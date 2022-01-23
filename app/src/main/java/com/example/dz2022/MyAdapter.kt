e5bd94425ce6f74be09b77e6a056ef5ac817c771
adizqx <nerozqx@gmail.com>
2022-01-23 16:47:06 +0600

    remove added

--- app/src/main/java/com/example/dz2022/MyAdapter.kt
+++ app/src/main/java/com/example/dz2022/MyAdapter.kt
@@ -1,25 +1,26 @@
 package com.example.dz2022
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 
-import android.content.Intent
 
-
-class MyAdapter(private var array: ArrayList<Data>,val context: FragmentActivity
+class MyAdapter(
+    private var array: ArrayList<Data>, val context: FragmentActivity
 ) : RecyclerView.Adapter<MyAdapter.MyVieholder>() {
     class MyVieholder(view: View) : RecyclerView.ViewHolder(view) {
-      var Name: TextView = view.findViewById(R.id.tvName)
-      var fDesc: TextView = view.findViewById(R.id.tvDesc)
-      var sDesc: TextView = view.findViewById(R.id.tvDesc2)
-      var fDate: TextView = view.findViewById(R.id.tvFirstDate)
-      var sDate: TextView = view.findViewById(R.id.tvSecDate)
-      var trud: TextView = view.findViewById(R.id.trud)
+        var Name: TextView = view.findViewById(R.id.tvName)
+        var fDesc: TextView = view.findViewById(R.id.tvDesc)
+        var sDesc: TextView = view.findViewById(R.id.tvDesc2)
+        var fDate: TextView = view.findViewById(R.id.tvFirstDate)
+        var sDate: TextView = view.findViewById(R.id.tvSecDate)
+        var trud: TextView = view.findViewById(R.id.trud)
+        var removeBtn: ImageButton = view.findViewById(R.id.removeBtn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyVieholder {
@@ -38,11 +39,14 @@ class MyAdapter(private var array: ArrayList<Data>,val context: FragmentActivity
         holder.sDate.text = "Дата2:${item.secDate}"
         holder.trud.text = "Трудности:${item.Trud}"
 
-
+        holder.removeBtn.setOnClickListener {
+            Constance.events.removeAt(ind)
+            notifyDataSetChanged()
+        }
 
         holder.itemView.setOnClickListener {
 
-            val i = Intent (context, SecondActivity::class.java)
+            val i = Intent(context, SecondActivity::class.java)
             i.putExtra("key", item)
             i.putExtra("key2", ind)
             context.startActivity(i)
@@ -56,12 +60,12 @@ class MyAdapter(private var array: ArrayList<Data>,val context: FragmentActivity
     }
 
 
-//fun update(){
+    //fun update(){
 //    notifyDataSetChanged()
 //}
-fun bindActors(newData: ArrayList<Data>) {
-    array = newData
-    notifyDataSetChanged()
-}
+    fun bindActors(newData: ArrayList<Data>) {
+        array = newData
+        notifyDataSetChanged()
+    }
 
 }
